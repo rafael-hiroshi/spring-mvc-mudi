0d7315cc9190b31e25ead32ced24a37a3e89b2ed
Rafael Hiroshi <rafaelhkp@gmail.com>
2021-11-12 09:47:36 -0300

    Find by status and format values

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,14 +1,19 @@
 package br.com.alura.mvc.mudi.controller;
 
 import br.com.alura.mvc.mudi.model.Order;
+import br.com.alura.mvc.mudi.model.OrderStatus;
 import br.com.alura.mvc.mudi.repository.OrderRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
     private final OrderRepository repository;
@@ -17,10 +22,23 @@ public class HomeController {
         this.repository = repository;
     }
 
-    @GetMapping("/home")
+    @GetMapping
     public String home(Model model) {
         List<Order> orders = repository.findAll();
         model.addAttribute("orders", orders);
         return "home";
+    }
+
+    @GetMapping("/{status}")
+    public String byStatus(@PathVariable("status") String status, Model model) {
+        List<Order> orders = repository.findByStatus(OrderStatus.valueOf(status.toUpperCase()));
+        model.addAttribute("orders", orders);
+        model.addAttribute("status", status);
+        return "home";
+    }
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public String onError() {
+        return "redirect:/home";
     }
 }
--- src/main/java/br/com/alura/mvc/mudi/repository/OrderRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package br.com.alura.mvc.mudi.repository;
 
 import br.com.alura.mvc.mudi.model.Order;
+import br.com.alura.mvc.mudi.model.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByStatus(OrderStatus waiting);
 }
